2754979fb3102cd616402fa88c529edd7ffbe5f1
ripudaman-cedcoss <ripudamanrajput@cedcommerce.com>
2023-05-10 16:12:48 +0530

    session time

--- src/module/auth.ts
+++ src/module/auth.ts
@@ -52,6 +52,6 @@ export const sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
         next()
     } else {
-        res.json({ message: "cookie expired " })
+        res.json({ message: "cookie expired ", sessionActive: false })
     }
 }
