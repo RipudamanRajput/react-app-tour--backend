44def7af5a0a2c73ad4c2a92cdf025c1f59b7130
ripudaman-cedcoss <ripudamanrajput@cedcommerce.com>
2023-05-18 02:45:51 +0530

    changes serve 1

--- src/server.ts
+++ src/server.ts
@@ -15,23 +15,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
-app.enable('trust proxy')
-app.set("trust proxy", 1);
-app.use(session(
-    {
-        key: "user_sid",
-        secret: 'randomstr',
-        resave: false,
-        proxy:true,
-        name:"tourappcookie",
-        saveUninitialized: false,
-        cookie: {
-            maxAge: 6000000,
-            sameSite: 'none',
-            secure: true 
-        }
-    }
-))
+app.enable('trust proxy');
+app.use(session({
+  key: "user_sid",
+  secret: 'randomstr',
+  resave: false,
+  proxy: true,
+  name: "tourappcookie",
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 6000000,
+    sameSite: 'none',
+    secure: true
+  }
+}));
 
 app.use(cors(
     {
